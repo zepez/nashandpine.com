dcbe5435df0492d4c9dfe515a3a51b2b082f0b74
Alex Zepezauer <zepezaueralex@gmail.com>
2021-09-27 12:01:59 -0400

    fix mobile logo. use the long logo

--- components/navigation/mobile.jsx
+++ components/navigation/mobile.jsx
@@ -8,7 +8,7 @@ export default function MobileNav() {
 			<nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
 				<div className="flex items-center mb-8">
 					<a className="mr-auto text-3xl font-semibold leading-none" href="#home">
-						<img className="h-10" src="logo.svg" alt="" width="auto" />
+						<img className="h-16 ml-4" src="/logos/nap/long.png" alt="" width="auto" />
 					</a>
 					<button className="navbar-close">
 						<svg className="h-6 w-6 text-blueGray-400 cursor-pointer hover:text-blueGray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
